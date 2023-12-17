8f94d371bdfffacbabc5579ff35884f827d578ac
unknown <msayem089@gmail.com>
2023-12-17 14:49:22 +0600

    single_user.js bugs fixed

--- api/users/single_user.js
+++ api/users/single_user.js
@@ -1,27 +1,26 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
-const client = require("../../db/client");
+const client = require('../../db/client');
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     // console.log(id);
-    req.body = req.query;
+    const filter = req.query;
     const get_single_user = await client.query(
-      "SELECT * FROM get_user($1,$2)",
-      [id, req.body]
+      'SELECT * FROM get_user($1,$2)',
+      [id, filter]
     );
     // console.log(get_single_user?.rows[0]?.get_user);
     let result = get_single_user?.rows[0]?.get_user?.status;
-    console.log(result);
-    if (result === "success") {
+    if (result === 'success') {
       res.status(200).json({ user: get_single_user?.rows[0]?.get_user?.user });
     } else {
-      res.status(200).json({ msg: "user not found" });
+      res.status(200).json({ msg: 'user not found' });
     }
     // const get_single_user=await client.query()
   } catch (e) {
-    res.status(200).json({ msg: "user not found" });
+    res.status(200).json({ msg: 'user not found' });
   }
 });
 
